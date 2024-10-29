6ca69aab853ee732022d475218ccd825a70f237e
michalk1924 <michalk1924@gmail.com>
2024-10-29 13:44:33 +0200

    first commit

---
+++ cards.js
@@ -0,0 +1,33 @@
+
+const Colors = {
+    red: "#ff0000",
+    blue: "#0000ff",
+    green: "#00ff00",
+    yellow: "#ffff00",
+    orange: "#ffa500",
+};
+
+const Cards = [
+    {
+        id: 0,
+        text: "text 1",
+        color: Colors.red,
+    },
+    {
+        id: 1,
+        text: "text 2",
+        color: Colors.blue,
+    },
+    {
+        id: 2,
+        text: "text 3",
+        color: Colors.green,
+    },
+    {
+        id: 3,
+        text: "text 4",
+        color: Colors.yellow,
+    }
+]
+
+module.exports = Cards;
---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const port = 8080;
+
+const Cards = require('./cards');
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get('/', async (req, res) => {
+    try {
+        res.send("Welcome");
+    }
+    catch (error) {
+        res.status(500).send({ message: "Internal Server Error" })
+    }
+});
+
+app.get('/api/cards', async (req, res) => {
+    try {
+        res.send(Cards);
+    }
+    catch (error) {
+        res.status(500).send({ message: "Internal Server Error" })
+    }
+});
+
+app.get('/api/cards/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        const card = Cards.find(card => card.id === id);
+        if (!card) {
+            res.status(404).send({ message: "Card not found" })
+        }
+        else {
+            res.send(card);
+        }
+    }
+    catch (error) {
+        res.status(500).send({ message: "Internal Server Error" })
+    }
+});
+
+app.post('/api/cards', async (req, res) => {
+    const newCard = {
+        id: Cards.length,
+        ...req.body
+    }
+    try {
+        Cards.push(newCard);
+        res.status(201).send(newCard);
+    }
+    catch (error) {
+        res.status(400).send({ message: "Invalid card data" })
+    }
+});
+
+app.put('/api/cards/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    const updatedCard = req.body;
+    try {
+        const card = Cards.find(card => card.id === id);
+        if (!card) {
+            res.status(404).send({ message: "Card not found" })
+        }
+        else {
+            Object.assign(card, updatedCard);
+            res.send(card);
+        }
+    }
+    catch (error) {
+        res.status(500).send({ message: "Internal Server Error" })
+    }
+});
+
+app.delete('/api/cards/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        const card = Cards.find(card => card.id === id);
+        if (!card) {
+            res.status(404).send({ message: "Card not found" })
+        }
+        else {
+            Cards.splice(id, 1);
+            res.send(card);
+        }
+    }
+    catch (error) {
+        res.status(500).send({ message: "Internal Server Error" })
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
